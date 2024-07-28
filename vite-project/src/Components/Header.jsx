307b5c1c5d694389381ad7bf03bda7fbc9afd9d5
Nethminijayani <jayaninethmini2000@gmail.com>
2024-07-29 00:04:58 +0530

    Changed the header image

--- vite-project/src/Components/Header.jsx
+++ vite-project/src/Components/Header.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-    <div className="min-h-screen flex flex-row justify-between items-center lg:px-32 px-5 bg-[url('./assets/HImg3.jpg')] bg-cover bg-no-repeat " id='header'>
+    <div className="min-h-screen flex flex-row justify-between items-center lg:px-32 px-5 bg-[url('./assets/HImg4.jpg')] bg-cover bg-no-repeat " id='header'>
         <div className='w-full lg:w-2/3 space-y-5'>
             <div className=' font-semibold text-6xl'>
 
             {/*text content section */}    
             <div className='space-y-2 text-center sm:text-left p-4'>
-            <h1 className='font-extrabold font-sans text-8xl bg-gradient-to-r from-green-400 to-primary bg-clip-text text-transparent'>Discover, Create, and Enjoy </h1>
+            <h1 className='font-extrabold font-sans text-8xl bg-gradient-to-r from-green-900 to-gray-700 bg-clip-text text-transparent'>Discover, Create, and Enjoy </h1>
             <br />
             <h2 className='font-semibold font-sans text-white text-3xl text-transparent bg-clip-text'>Explore our collection of delicious recipes, cooking tips, and culinary inspiration.</h2>
             </div>
--- vite-project/src/Components/Newsletter.jsx
+++ vite-project/src/Components/Newsletter.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 const Newsletter = () => {
   return (
-<div class="my-24 w-full flex flex-col items-center md:px-15   ">
-      <div class=" w-full rounded-2xl border shadow-x1 p-10 bg-gradient-to-r from-green-200 to-white">
-        <div class="flex flex-col items-center space-y-4 ">
-          <h1 class="font-bold text-2xl text-gray-700 w-4/6 text-center">
+<div className="my-24 w-full flex flex-col items-center md:px-15   ">
+      <div className=" w-full rounded-2xl border shadow-x1 p-10 bg-gradient-to-r from-green-200 to-white">
+        <div className="flex flex-col items-center space-y-4 ">
+          <h1 className="font-bold text-2xl text-gray-700 w-4/6 text-center">
            SUBSCRIBE
           </h1>
-          <p class="text-sm text-gray-500 text-center ">
-            Hello, please enter your email address to subscribe our newsletters.
+          <p className="text-m text-gray-500 text-center ">
+             Enter your email to subscribe our newsletters.
           </p>
           <input
             type="text"
             placeholder="Email"
-            class="border-2 rounded-lg h-12 px-4"
+            className=" border-2 rounded-lg h-10 w-35 px-4"
           />
           <button
-            class=" flex justify-center text-xl text-black font-[poppins] py-2 px-6 rounded md:ml-8 hover:bg-white hover:text-lime-400 duration-500 border-2"
+            className=" flex justify-center bg-green-800 text-xl text-black font-[poppins] py-2 px-6 rounded md:ml-8 hover:bg-white hover:text-lime-400 duration-500 border-2"
           >
             Submit
           </button>
